6007fe29d035a61e7df857478db96863e52f45a2
Matthieu Robin <robin10mat@gmail.com>
2014-03-29 13:28:43 +0100

    MAJ BDD table taglink : 'id' est devenu 'idLink' + table link : lindate est par défaut à CURRRENT_TIMESTAMP

--- mockup/alienore.sql
+++ mockup/alienore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mer 26 Mars 2014 à 16:09
+-- Généré le: Sam 29 Mars 2014 à 12:27
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.16
 
@@ -30,12 +30,12 @@ USE `alienore`;
 
 CREATE TABLE IF NOT EXISTS `link` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `linkdate` datetime NOT NULL,
+  `linkdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `title` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -56,9 +56,9 @@ CREATE TABLE IF NOT EXISTS `tag` (
 --
 
 CREATE TABLE IF NOT EXISTS `taglink` (
-  `id` int(11) NOT NULL,
+  `idLink` int(11) NOT NULL,
   `idTag` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`idTag`),
+  PRIMARY KEY (`idLink`,`idTag`),
   KEY `FK_TAGLINK_id_TAG` (`idTag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(20) NOT NULL,
   `hash` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Contraintes pour les tables exportées
@@ -84,8 +84,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Contraintes pour la table `taglink`
 --
 ALTER TABLE `taglink`
-  ADD CONSTRAINT `FK_TAGLINK_id_TAG` FOREIGN KEY (`idTag`) REFERENCES `tag` (`id`),
-  ADD CONSTRAINT `FK_TAGLINK_id` FOREIGN KEY (`id`) REFERENCES `link` (`id`);
+  ADD CONSTRAINT `FK_TAGLINK_id` FOREIGN KEY (`idLink`) REFERENCES `link` (`id`),
+  ADD CONSTRAINT `FK_TAGLINK_id_TAG` FOREIGN KEY (`idTag`) REFERENCES `tag` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
