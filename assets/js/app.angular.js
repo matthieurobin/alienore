d82433044eeb510650ca6fc258015af9c24a259a
matthieurobin <robin10mat@gmail.com>
2014-07-27 10:38:15 +0200

    classe css pour différencier les tags sélectionnés de ceux qui ne le sont pas

--- assets/js/app.angular.js
+++ assets/js/app.angular.js
@@ -24,28 +24,28 @@
           innerObj = {};
           innerObj[fullSubName] = subValue;
           query += param(innerObj) + '&';
+        }
       }
-  }
-  else if(value instanceof Object) {
-    for(subName in value) {
-      subValue = value[subName];
-      fullSubName = name + '[' + subName + ']';
-      innerObj = {};
-      innerObj[fullSubName] = subValue;
-      query += param(innerObj) + '&';
-  }
-}
-else if(value !== undefined && value !== null)
-    query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-}
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
 
-return query.length ? query.substr(0, query.length - 1) : query;
-};
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
 
   // Override $http service's default transformRequest
   $httpProvider.defaults.transformRequest = [function(data) {
     return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-}];
+  }];
 });
 
 var app = angular.module('alienore', ['postModule']);
@@ -53,10 +53,10 @@ var app = angular.module('alienore', ['postModule']);
 /**
  * filtre pour permettre l'affichage des entités html
  */
-app.filter('unsafe', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
+ app.filter('unsafe', function($sce) {
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
 });
 
 /*
@@ -91,8 +91,8 @@ app.controller('mainCtrl', function($scope, $http){
       $scope.token = data.token;
       $scope.nbLinks = data.nbLinks;
       $scope.pagination = 'default';
-  });
-};
+    });
+  };
 
 //initialiser le scope
 $scope.init = function(){
@@ -103,8 +103,8 @@ $scope.init = function(){
     $http.get('?c=tags&a=data_all')
     .success(function(data){
       $scope.tags = data.tags;
-  });
-};
+    });
+  };
 
 //initialisation du scope
 $scope.init();
@@ -114,7 +114,7 @@ $scope.init();
  * @param  {int} linkId 
  * @param  {string} editString : titre à remplacer dans la modal
  */
-$scope.editLink = function(linkId, editString){
+ $scope.editLink = function(linkId, editString){
     //on réinitialise le formulaire
     $scope.formDataLink = {}
     $('#tagBox').tagging('reset');
@@ -122,28 +122,28 @@ $scope.editLink = function(linkId, editString){
 
     $http.get('?c=links&a=data_get&linkId=' + linkId)
     .success(function(data) {
-        $scope.formDataLink = data.link;
-        var _tags = [];
-        for (_i = 0; _i < data.tags.length; ++_i) {
-            $('#tagBox').tagging("add", data.tags[_i].label);
-        }
+      $scope.formDataLink = data.link;
+      var _tags = [];
+      for (_i = 0; _i < data.tags.length; ++_i) {
+        $('#tagBox').tagging("add", data.tags[_i].label);
+      }
     });
     $('#modal-link').modal('show');
-};
+  };
 
 /**
  * action submit le formulaire d'édition/d'ajout d'un lien
  * @return {object} retourne le lien + les tags (ajoutés/supprimés/nouveaux(BDD)/ceux qui n'ont pas été modifiés)
  */
-$scope.submitLink = function (){
-    $scope.formDataLink.tags = $('#tagBox').tagging('getTags');
-    var _url = '?c=links&a=data_saved';
+ $scope.submitLink = function (){
+  $scope.formDataLink.tags = $('#tagBox').tagging('getTags');
+  var _url = '?c=links&a=data_saved';
     //si c'est une édition
     if($scope.formDataLink.id){
       _url += '&linkId=' + $scope.formDataLink.id;
-  }
-  $http.post(_url, $scope.formDataLink)
-  .success(function(data) {
+    }
+    $http.post(_url, $scope.formDataLink)
+    .success(function(data) {
     //cas d'un nouveau lien
     if(!$scope.formDataLink.id){
       //on cherche à afficher le lien et ses tags
@@ -193,8 +193,8 @@ $scope.submitLink = function (){
       for(var i = 0; i < _nbLinksAdded; ++i){
         var _tag = data.tags.added[i];
         var _tagDom = '<div id="tag-' + _tag.id + '" class="tag tag-list">' + 
-          '<a class="a-tag pointer" ng-click="selectTag('+ _tag.id +')"><span>#</span>' + _tag.label + '</a></span>' +
-          '</div>';
+        '<a class="a-tag pointer" ng-click="selectTag('+ _tag.id +')"><span>#</span>' + _tag.label + '</a></span>' +
+        '</div>';
         $link.find('.tags').append(_tagDom);
       }
       //MAJ des tags dans la sidebar
@@ -218,22 +218,22 @@ $scope.submitLink = function (){
  * action de suppresion d'un lien
  * @param  {int} linkId
  */
-$scope.deleteLink = function(linkId){
-    $http.get('?c=links&a=data_delete&t=' + $scope.token + '&id=' + linkId)
-    .success(function(data){
-        $('#link-' + linkId).fadeOut(400, function(){
-          $scope.nbLinks -=1;
-          $(this).remove();
-          showAlert(data.text, 'modal-helper-green');
-        });
+ $scope.deleteLink = function(linkId){
+  $http.get('?c=links&a=data_delete&t=' + $scope.token + '&id=' + linkId)
+  .success(function(data){
+    $('#link-' + linkId).fadeOut(400, function(){
+      $scope.nbLinks -=1;
+      $(this).remove();
+      showAlert(data.text, 'modal-helper-green');
     });
+  });
 };
 
 /**
  * édition d'un tag
  * @param  {int} tagId 
  */
-$scope.editTag = function(tagId){
+ $scope.editTag = function(tagId){
   $http.get('?c=tags&a=data_form&tagId=' + tagId)
   .success(function(data){
     $scope.formDataTag.label = data.label;
@@ -245,13 +245,12 @@ $scope.editTag = function(tagId){
  * soumission du formulaire d'édition d'un tag
  * @return {object} nouveau tag + message à afficher
  */
-$scope.submitTag = function(){
+ $scope.submitTag = function(){
   $http.post('?c=tags&a=data_saved&tagId=' + $scope.formDataTag.id, $scope.formDataTag)
   .success(function(data){
     if(data.saved){
       //MAJ le dom
       $('#tag-' + $scope.formDataTag.id + ' .tag-label ').html(data.tag.label);
-      console.log($('.link-tag-' + $scope.formDataTag.id + ' .tag-label '));
       $('.link-tag-' + $scope.formDataTag.id + ' .tag-label ').each(function(){
         $(this).html(data.tag.label);
       });
@@ -268,7 +267,7 @@ $scope.submitTag = function(){
  * @param  {array} tags : tableau des tags sélectionnés
  * @return {object} objet contenant les liens recherchés + le nombre de page de la recherche + le nombre de lien trouvés
  */
-$scope.getLinksByTags = function(tags){
+ $scope.getLinksByTags = function(tags){
   $http.get('?c=links&a=data_getLinksByTags&tagsId=' + tags.toString())
   .success(function(data){
     $scope.links = data.links;
@@ -286,30 +285,37 @@ $scope.getLinksByTags = function(tags){
  $scope.selectTag = function(tagId){
     //on vérifie que le lien n'est pas déjà dans la barre de recherche
     var nbTags = $scope.tagsSelected.length;
-    var find = false;
-    var tags = [];
-    for(var i = 0; i < nbTags; ++i){
-      if($scope.tagsSelected[i].id === tagId){
-        find = true;
+    //si il y a plus de trois tags sélectionnés on remplace le dernier
+    if(nbTags !== 3){
+      var find = false;
+      var tags = [];
+      for(var i = 0; i < nbTags; ++i){
+        if($scope.tagsSelected[i].id === tagId){
+          find = true;
+        }
+        tags.push($scope.tagsSelected[i].id);
       }
-      tags.push($scope.tagsSelected[i].id);
-    }
-    if(!find){
+      if(!find){
       //on ajoute le tag à la barre de recherche
       $http.get('?c=tags&a=data_get&tagId=' + tagId)
-        .success(function(data){
-          $scope.tagsSelected.push(data);
-          tags.push(tagId);
+      .success(function(data){
+        $scope.tagsSelected.push(data);
+        tags.push(tagId);
           //on cherche les liens
           $scope.getLinksByTags(tags);
-      });
+        });
+      }
+      //on ajoute la classe css 
+      $('#tag-' + tagId + ' .tag-label').addClass('tags-active');
+    }else{
+      showAlert('You already have 3 tags selected, deselect one of them or all','modal-helper-red');
     }
 };
 
 /**
  * chercher les liens pour la page suivante
  */
-$scope.nextPage = function(){
+ $scope.nextPage = function(){
   $scope.currentPage += 1;
     //si on est arrivé à la limite
     if ($scope.currentPage > $scope.limit) {
@@ -317,32 +323,32 @@ $scope.nextPage = function(){
     } else {
       switch ($scope.pagination) {
         case 'default':
-          $http.get('?c=links&a=data_all&page=' + $scope.currentPage)
-          .success(function(data){
-            $scope.links = $scope.links.concat(data.links);
-          });
-          break;
+        $http.get('?c=links&a=data_all&page=' + $scope.currentPage)
+        .success(function(data){
+          $scope.links = $scope.links.concat(data.links);
+        });
+        break;
         case 'tags':
-          $http.get('?c=links&a=data_getLinksByTag&page=' + $scope.currentPage + '&tagId=' + $scope.tagsSelected)
-          .success(function(data){
-            $scope.links = $scope.links.concat(data.links);
-          });
-          break;
+        $http.get('?c=links&a=data_getLinksByTag&page=' + $scope.currentPage + '&tagId=' + $scope.tagsSelected)
+        .success(function(data){
+          $scope.links = $scope.links.concat(data.links);
+        });
+        break;
         case 'search':
-          $http.get('?c=links&a=data_search&page=' + $scope.currentPage + '&search=' + $scope.search)
-          .success(function(data){
-            $scope.links = $scope.links.concat(data.links);
-          });
-          break;
+        $http.get('?c=links&a=data_search&page=' + $scope.currentPage + '&search=' + $scope.search)
+        .success(function(data){
+          $scope.links = $scope.links.concat(data.links);
+        });
+        break;
       }
     }
-};
+  };
 
 /**
  * soumission du formulaire de recherche
  * @return {object} 
  */
-$scope.submitSearch = function(){
+ $scope.submitSearch = function(){
   //la recherche avec des tags sélectionnés n'est pas implémentée
   $scope.tagsSelected = [];
 
@@ -362,7 +368,7 @@ $scope.submitSearch = function(){
 /**
  * supprimer la recherche
  */
-$scope.removeSearch = function(){
+ $scope.removeSearch = function(){
   $scope.search = undefined;
   $('#input-search').val('').blur();
   //on cherche tous les tags
@@ -373,7 +379,7 @@ $scope.removeSearch = function(){
  * supprimer le tag des tags sélectionnés
  * @param  {int} tagId
  */
-$scope.removeSelectedTag = function(tagId){
+ $scope.removeSelectedTag = function(tagId){
   //s'il reste des tags sélectionnés
   if($scope.tagsSelected.length > 1){
     var nbTags = $scope.tagsSelected.length;
@@ -397,6 +403,7 @@ $scope.removeSelectedTag = function(tagId){
     $scope.tagsSelected = [];
     $scope.getLinks();
   }
+  $('#tag-' + tagId + ' .tag-label').removeClass('tags-active');
 };
 
 });
